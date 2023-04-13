9c076250eb23228d3277e4e648281254a60d2e2b
Stalloyde <stalloyde93@gmail.com>
2023-04-13 22:59:07 +0800

    Add test computer random attacks

--- src/player/player.test.js
+++ src/player/player.test.js
@@ -10,8 +10,50 @@ test('player attack', () => {
   expect(player.attack([1, 1])).toBe('Cannot attack same coordinates twice');
 });
 
-test('computer attack', () => {
-  expect(computer.attack([1, 1])).toBe(4);
-  expect(computer.attack([1, 2])).toBe(3);
-  expect(computer.attack([1, 1])).toBe('Cannot attack same coordinates twice');
+test('computer attack with random coordinate', () => {
+  expect([
+    'Missed!',
+    4,
+    3,
+    2,
+    1,
+    'Cannot attack same coordinates twice',
+    'Your ship has been sunk!',
+  ]).toContain(computer.attack());
+  expect([
+    'Missed!',
+    4,
+    3,
+    2,
+    1,
+    'Cannot attack same coordinates twice',
+    'Your ship has been sunk!',
+  ]).toContain(computer.attack());
+  expect([
+    'Missed!',
+    4,
+    3,
+    2,
+    1,
+    'Cannot attack same coordinates twice',
+    'Your ship has been sunk!',
+  ]).toContain(computer.attack());
+  expect([
+    'Missed!',
+    4,
+    3,
+    2,
+    1,
+    'Cannot attack same coordinates twice',
+    'Your ship has been sunk!',
+  ]).toContain(computer.attack());
+  expect([
+    'Missed!',
+    4,
+    3,
+    2,
+    1,
+    'Cannot attack same coordinates twice',
+    'Your ship has been sunk!',
+  ]).toContain(computer.attack());
 });
