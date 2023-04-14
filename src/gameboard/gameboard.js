5d173602f0aa6ea2b1c59497cb0f6dfaad31c543
Stalloyde <stalloyde93@gmail.com>
2023-04-14 13:53:31 +0800

    Refactor createGrid()

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -5,109 +5,13 @@ import shipsFactory from '../ships/ship';
 
 function gameboardFactory(name = 'Computer') {
   function createGrid() {
-    const grid = [
-      '1,1',
-      '1,2',
-      '1,3',
-      '1,4',
-      '1,5',
-      '1,6',
-      '1,7',
-      '1,8',
-      '1,9',
-      '1,10',
-      '2,1',
-      '2,2',
-      '2,3',
-      '2,4',
-      '2,5',
-      '2,6',
-      '2,7',
-      '2,8',
-      '2,9',
-      '2,10',
-      '3,1',
-      '3,2',
-      '3,3',
-      '3,4',
-      '3,5',
-      '3,6',
-      '3,7',
-      '3,8',
-      '3,9',
-      '3,10',
-      '4,1',
-      '4,2',
-      '4,3',
-      '4,4',
-      '4,5',
-      '4,6',
-      '4,7',
-      '4,8',
-      '4,9',
-      '4,10',
-      '5,1',
-      '5,2',
-      '5,3',
-      '5,4',
-      '5,5',
-      '5,6',
-      '5,7',
-      '5,8',
-      '5,9',
-      '5,10',
-      '6,1',
-      '6,2',
-      '6,3',
-      '6,4',
-      '6,5',
-      '6,6',
-      '6,7',
-      '6,8',
-      '6,9',
-      '6,10',
-      '7,1',
-      '7,2',
-      '7,3',
-      '7,4',
-      '7,5',
-      '7,6',
-      '7,7',
-      '7,8',
-      '7,9',
-      '7,10',
-      '8,1',
-      '8,2',
-      '8,3',
-      '8,4',
-      '8,5',
-      '8,6',
-      '8,7',
-      '8,8',
-      '8,9',
-      '8,10',
-      '9,1',
-      '9,2',
-      '9,3',
-      '9,4',
-      '9,5',
-      '9,6',
-      '9,7',
-      '9,8',
-      '9,9',
-      '9,10',
-      '10,1',
-      '10,2',
-      '10,3',
-      '10,4',
-      '10,5',
-      '10,6',
-      '10,7',
-      '10,8',
-      '10,9',
-      '10,10',
-    ];
-    return grid;
+    const gridsArray = [];
+    for (let y = 1; y < 11; y++) {
+      for (let x = 1; x < 11; x++) {
+        gridsArray.push([`x:${x},y:${y}`]);
+      }
+    }
+    return gridsArray;
   }
 
   const missedShots = [];
@@ -147,11 +51,11 @@ function gameboardFactory(name = 'Computer') {
   const submarine = shipsFactory(3);
   const patrolBoat = shipsFactory(2);
 
-  placeShip(carrier, 'vertical', [1, 1]);
-  placeShip(battleship, 'vertical', [2, 1]);
-  placeShip(destroyer, 'vertical', [3, 1]);
-  placeShip(submarine, 'vertical', [4, 1]);
-  placeShip(patrolBoat, 'vertical', [5, 1]);
+  placeShip(carrier, 'vertical', [1, 1]); //1,1...1,2...1,3...1,4...1,5
+  placeShip(battleship, 'vertical', [2, 1]); //2,1...2,2...2,3...2,4...2,5
+  placeShip(destroyer, 'vertical', [3, 1]); //3,1...3,2...3,3
+  placeShip(submarine, 'vertical', [4, 1]); //4,1...4,2...4,3
+  placeShip(patrolBoat, 'vertical', [5, 1]); //5,1...5,2
 
   const allShips = {
     carrier,
@@ -178,4 +82,5 @@ function gameboardFactory(name = 'Computer') {
   }
   return { createGrid, placeShip, receiveAttack, hitShots, missedShots };
 }
+
 export default gameboardFactory;
