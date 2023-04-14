e49d8406a05959e3171574d3bd4bcb13fd70eab9
Stalloyde <stalloyde93@gmail.com>
2023-04-14 14:06:40 +0800

    Add grids

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import _ from 'lodash';
+import _, { create } from 'lodash';
 import shipsFactory from './ships/ship';
+import gameboardFactory from './gameboard/gameboard';
+import playerFactory from './player/player';
 import './style.css';
+
+const playerGameboardContainer = document.querySelector(
+  '.player-gameboard-container'
+);
+const computerGameboardContainer = document.querySelector(
+  '.computer-gameboard-container'
+);
+const computerGameboard = gameboardFactory();
+const playerGameboard = gameboardFactory('Stalloyde');
+
+function appendGrid(containerToAppendOn, gameboardToCreateGridFrom) {
+  const gridContainer = document.createElement('div');
+
+  gameboardToCreateGridFrom === playerGameboard
+    ? gridContainer.classList.add('player-grid')
+    : gridContainer.classList.add('computer-grid');
+  containerToAppendOn.appendChild(gridContainer);
+
+  const gridsArray = gameboardToCreateGridFrom.createGrid();
+  gridsArray.forEach((item) => {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.id = item;
+    gridContainer.appendChild(grid);
+  });
+}
+
+appendGrid(computerGameboardContainer, computerGameboard);
+appendGrid(playerGameboardContainer, playerGameboard);
