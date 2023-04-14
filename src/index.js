67f16d858ff0c0379efd4ca0cf7de6174b10a166
Stalloyde <stalloyde93@gmail.com>
2023-04-14 15:23:52 +0800

    WIP: Adding x&y axis labels

--- src/index.js
+++ src/index.js
@@ -10,8 +10,33 @@ const playerGameboardContainer = document.querySelector(
 const computerGameboardContainer = document.querySelector(
   '.computer-gameboard-container'
 );
+const xAxisLabel = document.querySelectorAll('.x-axis-label');
+const yAxisLabel = document.querySelectorAll('.y-axis-label');
 const computerGameboard = gameboardFactory();
 const playerGameboard = gameboardFactory('Stalloyde');
+
+function appendXAxisLabel() {
+  xAxisLabel.forEach((item) => {
+    for (let x = 1; x < 11; x++) {
+      const xLabel = document.createElement('div');
+      xLabel.textContent = `${String.fromCharCode(x + 64)}`;
+      item.appendChild(xLabel);
+    }
+  });
+}
+
+function appendYAxisLabel() {
+  yAxisLabel.forEach((item) => {
+    for (let y = 1; y < 11; y++) {
+      const yLabel = document.createElement('div');
+      yLabel.textContent = y;
+      item.appendChild(yLabel);
+    }
+  });
+}
+
+appendXAxisLabel();
+appendYAxisLabel();
 
 function appendGrid(containerToAppendOn, gameboardToCreateGridFrom) {
   const gridContainer = document.createElement('div');
