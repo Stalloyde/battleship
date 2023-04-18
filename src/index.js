0bc0f09285338034a86b639e796d117181b7e4c8
Stalloyde <stalloyde93@gmail.com>
2023-04-18 15:00:36 +0800

    Enable ships to be placed on player's gameboard on DOM

--- src/index.js
+++ src/index.js
@@ -57,4 +57,39 @@ appendXAxisLabel();
 appendYAxisLabel();
 appendGrid(computerGameboardContainer, computerGameboard);
 appendGrid(playerGameboardContainer, playerGameboard);
-appendShip();
+
+const carrier = playerGameboard.placeShip(
+  playerGameboard.allShips.carrier,
+  'vertical',
+  ['A', 1]
+); //A,1...A,2...A,3...A,4...A,5
+
+const battleship = playerGameboard.placeShip(
+  playerGameboard.allShips.battleship,
+  'vertical',
+  ['E', 5]
+); //E,5...E,6...E,7...E,8
+
+const destroyer = playerGameboard.placeShip(
+  playerGameboard.allShips.destroyer,
+  'horizontal',
+  ['C', 10]
+); //C,10...D,10..D,10
+
+const submarine = playerGameboard.placeShip(
+  playerGameboard.allShips.submarine,
+  'horizontal',
+  ['H', 6]
+); //H,6..I,6...J,6
+
+const patrolBoat = playerGameboard.placeShip(
+  playerGameboard.allShips.patrolBoat,
+  'horizontal',
+  ['H', 1]
+); //E,1...E,2
+
+appendShip(carrier, playerGameboard);
+appendShip(battleship, playerGameboard);
+appendShip(destroyer, playerGameboard);
+appendShip(submarine, playerGameboard);
+appendShip(patrolBoat, playerGameboard);
