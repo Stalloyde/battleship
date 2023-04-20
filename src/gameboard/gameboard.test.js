2b4fb2ea7d699795c2cef2540c469698972183b8
Stalloyde <stalloyde93@gmail.com>
2023-04-20 22:06:46 +0800

    Add create 2D array & occupy 2D array tests

--- src/gameboard/gameboard.test.js
+++ src/gameboard/gameboard.test.js
@@ -59,69 +59,83 @@ test('place ship horizontal', () => {
   ]);
 });
 
-// test('no overlapping ship placement', () => {
-//   const shipA = shipsFactory(5);
-//   const shipB = shipsFactory(5);
+test('no overlapping ship placement', () => {
+  const shipA = shipsFactory(5);
+  const shipB = shipsFactory(5);
 
-//   expect(gameboard.placeShip(shipA, 'horizontal', ['F', 6])).toEqual([
-//     ['F', 6],
-//     ['G', 6],
-//     ['H', 6],
-//     ['I', 6],
-//     ['J', 6],
-//   ]);
+  expect(gameboard.placeShip(shipA, 'horizontal', ['F', 6])).toEqual([
+    ['F', 6],
+    ['G', 6],
+    ['H', 6],
+    ['I', 6],
+    ['J', 6],
+  ]);
 
-//   expect(gameboard.placeShip(shipB, 'horizontal', ['F', 6])).toBe(
-//     'Ship position overlaps another'
-//   );
-// });
+  expect(gameboard.placeShip(shipB, 'horizontal', ['F', 6])).toBe(
+    'Ship position overlaps another'
+  );
+});
 
-// test('no placement that exceeds board size', () => {
-//   const shipA = shipsFactory(5);
-//   expect(gameboard.placeShip(shipA, 'horizontal', ['G', 0])).toBe(
-//     'Error. Ship placement exceeds board size'
-//   );
+test('no placement that exceeds board size', () => {
+  const shipA = shipsFactory(5);
+  expect(gameboard.placeShip(shipA, 'horizontal', ['G', 0])).toBe(
+    'Error. Ship placement exceeds board size'
+  );
 
-//   expect(gameboard.placeShip(shipA, 'horizontal', ['H', 1])).toBe(
-//     'Error. Ship placement exceeds board size'
-//   );
+  expect(gameboard.placeShip(shipA, 'horizontal', ['H', 1])).toBe(
+    'Error. Ship placement exceeds board size'
+  );
 
-//   expect(gameboard.placeShip(shipA, 'horizontal', ['H', 8])).toBe(
-//     'Error. Ship placement exceeds board size'
-//   );
+  expect(gameboard.placeShip(shipA, 'horizontal', ['H', 8])).toBe(
+    'Error. Ship placement exceeds board size'
+  );
 
-//   expect(gameboard.placeShip(shipA, 'horizontal', ['G', 7])).toBe(
-//     'Error. Ship placement exceeds board size'
-//   );
+  expect(gameboard.placeShip(shipA, 'horizontal', ['G', 7])).toBe(
+    'Error. Ship placement exceeds board size'
+  );
 
-//   expect(gameboard.placeShip(shipA, 'vertical', ['E', 7])).toBe(
-//     'Error. Ship placement exceeds board size'
-//   );
-// });
+  expect(gameboard.placeShip(shipA, 'vertical', ['E', 7])).toBe(
+    'Error. Ship placement exceeds board size'
+  );
+});
 
-// test('receive attack hit', () => {
-//   expect(gameboard.receiveAttack(['B', 1])).toBe(3);
-//   expect(gameboard.receiveAttack(['B', 2])).toBe(2);
-//   expect(gameboard.receiveAttack(['B', 3])).toBe(1);
-//   expect(gameboard.receiveAttack(['B', 4])).toBe('Your ship has been sunk!');
-//   expect(gameboard.receiveAttack(['E', 1])).toBe(1);
-//   expect(gameboard.receiveAttack(['E', 2])).toBe('Your ship has been sunk!');
-// });
+test('receive attack hit', () => {
+  expect(gameboard.receiveAttack(['B', 1])).toBe(3);
+  expect(gameboard.receiveAttack(['B', 2])).toBe(2);
+  expect(gameboard.receiveAttack(['B', 3])).toBe(1);
+  expect(gameboard.receiveAttack(['B', 4])).toBe('Your ship has been sunk!');
+  expect(gameboard.receiveAttack(['E', 1])).toBe(1);
+  expect(gameboard.receiveAttack(['E', 2])).toBe('Your ship has been sunk!');
+});
 
-// test('receive attack miss', () => {
-//   expect(gameboard.receiveAttack(['G', 1])).toBe('Missed!');
-//   expect(gameboard.receiveAttack(['G', 2])).toBe('Missed!');
-//   expect(gameboard.receiveAttack(['G', 3])).toBe('Missed!');
-//   expect(gameboard.receiveAttack(['G', 4])).toBe('Missed!');
-//   expect(gameboard.receiveAttack(['I', 1])).toBe('Missed!');
-//   expect(gameboard.receiveAttack(['I', 2])).toBe('Missed!');
-// });
+test('receive attack miss', () => {
+  expect(gameboard.receiveAttack(['G', 1])).toBe('Missed!');
+  expect(gameboard.receiveAttack(['G', 2])).toBe('Missed!');
+  expect(gameboard.receiveAttack(['G', 3])).toBe('Missed!');
+  expect(gameboard.receiveAttack(['G', 4])).toBe('Missed!');
+  expect(gameboard.receiveAttack(['I', 1])).toBe('Missed!');
+  expect(gameboard.receiveAttack(['I', 2])).toBe('Missed!');
+});
+
+test('Create 2D array', () => {
+  expect(gameboard.createGrid()).toEqual([
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
+  ]);
+});
 
 test('Occupy 2D array', () => {
   const shipA = shipsFactory(5);
+  gameboard.placeShip(shipA, 'vertical', ['A', 3]);
 
-  gameboard.placeShip(shipA, 'vertical', ['A', 3]); //['A', 3], ['A', 4], ['A', 5], ['A', 6], ['A', 7]
-  gameboard.createGrid();
   expect(gameboard.gridsArray).toEqual([
     [[], [], [], [], [], [], [], [], [], []],
     [[], [], [], [], [], [], [], [], [], []],
@@ -135,31 +149,33 @@ test('Occupy 2D array', () => {
     [[], [], [], [], [], [], [], [], [], []],
   ]);
 
-  gameboard.placeShip(shipA, 'vertical', ['G', 6]); //['G', 6, ['G', 7], ['G', 8], ['G', 9], ['G', 10]
+  const shipB = shipsFactory(4);
+  gameboard.placeShip(shipB, 'vertical', ['G', 6]);
   expect(gameboard.gridsArray).toEqual([
     [[], [], [], [], [], [], [], [], [], []],
     [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], ['G', 6], [], [], []],
-    [[], [], [], [], [], [], ['G', 7], [], [], []],
+    [['A', 3], [], [], [], [], [], [], [], [], []],
+    [['A', 4], [], [], [], [], [], [], [], [], []],
+    [['A', 5], [], [], [], [], [], [], [], [], []],
+    [['A', 6], [], [], [], [], [], ['G', 6], [], [], []],
+    [['A', 7], [], [], [], [], [], ['G', 7], [], [], []],
     [[], [], [], [], [], [], ['G', 8], [], [], []],
     [[], [], [], [], [], [], ['G', 9], [], [], []],
-    [[], [], [], [], [], [], ['G', 10], [], [], []],
+    [[], [], [], [], [], [], [], [], [], []],
   ]);
 
-  gameboard.placeShip(shipA, 'horizontal', ['C', 3]); //['C', 3, ['D', 3], ['E', 3], ['F', 3], ['G', 3]
+  const shipC = shipsFactory(3);
+  gameboard.placeShip(shipC, 'horizontal', ['C', 3]);
   expect(gameboard.gridsArray).toEqual([
     [[], [], [], [], [], [], [], [], [], []],
     [[], [], [], [], [], [], [], [], [], []],
-    [[], [], ['C', 3], ['D', 3], ['E', 3], ['F', 3], ['G', 3], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], [], [], []],
+    [['A', 3], [], ['C', 3], ['D', 3], ['E', 3], [], [], [], [], []],
+    [['A', 4], [], [], [], [], [], [], [], [], []],
+    [['A', 5], [], [], [], [], [], [], [], [], []],
+    [['A', 6], [], [], [], [], [], ['G', 6], [], [], []],
+    [['A', 7], [], [], [], [], [], ['G', 7], [], [], []],
+    [[], [], [], [], [], [], ['G', 8], [], [], []],
+    [[], [], [], [], [], [], ['G', 9], [], [], []],
     [[], [], [], [], [], [], [], [], [], []],
   ]);
 });
