3d0c7e9fcdff1293e51feadcd2f7338e305c9b90
Stalloyde <stalloyde93@gmail.com>
2023-05-06 22:50:51 +0800

    Remove unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import _, { create } from 'lodash';
-import shipsFactory from './ships/ship';
-import gameboardFactory from './gameboard/gameboard';
-import playerFactory from './player/player';
 import game from './UI/UI';
-import './style.css';
-
-game();
